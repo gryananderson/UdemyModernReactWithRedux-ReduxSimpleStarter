a19b3f98d3418ea066ec00a59b5ae693bb147d92
Ryan <gryananderson@gmail.com>
2018-04-08 14:11:09 -0500

    Modern Reach w Redux - Section 1, lesson 17

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        };
+    }
+
     render() {
         return <input onChange={(event) => console.log(event.target.value) } />;
     }
